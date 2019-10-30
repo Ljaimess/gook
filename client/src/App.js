5070a391b49e265158e4b4ab9e80e72b63591c8b
Lauren <ljaimes92@gmail.com>
2019-10-29 23:09:20 -0400

    did file structure

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// or imrc for react component
+import './App.css';
+
+// cc to create component
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
